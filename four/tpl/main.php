fa70928b1f6aaef05a2a82d41228e0b1cb393b09
masted <masted311@gmail.com>
2015-03-25 22:16:31 +0300

    Auto-commit on 25.03.2015 23:16:31

--- four/tpl/main.php
+++ four/tpl/main.php
@@ -51,19 +51,6 @@
     <? $this->tpl($d['layout'], $d) ?>
   </div>
   <div class="cBottom">
-    <div style="height: 0; overflow: hidden">
-    <!--LiveInternet counter--><script type="text/javascript"><!--
-      document.write("<a href='//www.liveinternet.ru/click' "+
-      "target=_blank><img src='//counter.yadro.ru/hit?t25.6;r"+
-      escape(document.referrer)+((typeof(screen)=="undefined")?"":
-      ";s"+screen.width+"*"+screen.height+"*"+(screen.colorDepth?
-        screen.colorDepth:screen.pixelDepth))+";u"+escape(document.URL)+
-      ";"+Math.random()+
-      "' alt='' title='LiveInternet: показано число посетителей за"+
-      " сегодня' "+
-      "border='0' width='88' height='15'><\/a>")
-      //--></script><!--/LiveInternet-->
-    </div>
   </div>
 </div>
 </body>
