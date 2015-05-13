d8b0bca473a19f1ba76f2248277633302152c7b9
masted <masted311@gmail.com>
2015-05-13 10:24:24 +0300

    Auto-commit on 13.05.2015 11:24:24

--- four/tpl/bookmarkContent.php
+++ four/tpl/bookmarkContent.php
@@ -1,8 +1,8 @@
 <div class="bookmarks">
-  <? $this->tpl('cp/links', $d['bookmarks']) ?>
+  <? $d['title'] ? print '<h1>'.$d['title'].'</h1>' : $this->tpl('cp/links', $d['bookmarks']) ?>
 </div>
 <div class="colBody">
-  <?= $this->tpl($d['contentTpl'], $d, false, false, 'contentTpl') ?>
+  <?= $d['contentTpl'] ? $this->tpl($d['contentTpl'], $d, false, false, 'contentTpl') : '<div class="colBodyContent">'.$d['html'].'</div>' ?>
 </div>
 <script>
   new Ngn.DdoItemsEdit();
