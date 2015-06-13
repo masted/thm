8ec9524fbfc4fb855c91b8a4f29f7473679cd265
masted <masted311@gmail.com>
2015-06-13 19:31:17 +0300

    Auto-commit on 13.06.2015 20:31:16

--- four/tpl/bookmarkContent.php
+++ four/tpl/bookmarkContent.php
@@ -1,5 +1,5 @@
 <div class="bookmarks">
-  <? $d['title'] ? print '<h1>'.$d['title'].'</h1>' : $this->tpl('cp/links', $d['bookmarks']) ?>
+  <? $d['pageTitle'] ? print '<h1>'.$d['pageTitle'].'</h1>' : $this->tpl('cp/links', $d['bookmarks']) ?>
 </div>
 <div class="colBody">
   <?= $d['contentTpl'] ? $this->tpl($d['contentTpl'], $d, false, false, 'contentTpl') : '<div class="colBodyContent">'.$d['html'].'</div>' ?>
