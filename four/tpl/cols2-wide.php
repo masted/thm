7d1c529e00a5535a4532a872734843fc6d97e568
masted <masted311@gmail.com>
2015-07-03 08:36:26 +0300

    Auto-commit on 03.07.2015 09:36:26

--- four/tpl/cols2-wide.php
+++ four/tpl/cols2-wide.php
@@ -1,7 +1,7 @@
 <div class="span-8 col1">
   <? $this->tpl($d['blocksTpl'], $d, false, false, 'blocks') ?>
 </div>
-<div class="span-13 col2 last">
+<div class="span-8 col2 last">
   <div class="bColBody">
     <? $this->tpl($d['tpl'], $d) ?>
   </div>
