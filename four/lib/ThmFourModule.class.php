f1e32fedd183ba19ec6d8cc1296bf2b682c381a0
masted <masted311@gmail.com>
2015-07-29 14:12:26 +0300

    Auto-commit on 29.07.2015 15:12:26

--- four/lib/ThmFourModule.class.php
+++ four/lib/ThmFourModule.class.php
@@ -7,7 +7,37 @@ class ThmFourModule {
   static function init($name, $baseParam = null) {
     self::$names[$baseParam] = $name;
     self::$basePaths[$name] = $baseParam;
-    Ngn::addBasePath(NGN_ENV_PATH.'/thm-four-modules/'.$name, 3);
+    Ngn::addBasePath(NGN_ENV_PATH.'/thm-four-modules/modules/'.$name, 3);
+  }
+
+  static function install($name) {
+    $structures = require NGN_ENV_PATH."/thm-four-modules/modules/$name/structures.php";
+    foreach ($structures as $strName => $fields) {
+      $fieldsManager = new DdFieldsManager($strName);
+      foreach ($fields as $field) {
+        if (($existingField = $fieldsManager->items->getItemByField('name', $field['name']))) {
+          
+          $f = Arr::filterByKeys($existingField, [
+            'oid',
+            'name',
+            'title',
+            'type',
+            'required',
+            'defaultDisallow',
+            'notList',
+            'editable',
+            'virtual',
+            'system',
+            'filterable'
+          ]);
+
+          $fieldsManager->update($existingField['id'], $f);
+
+        } else {
+          $fieldsManager->create($field);
+        }
+      }
+    }
   }
 
 }
