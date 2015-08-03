faa06bf3b25e1902db6a2f576230f8097f6896dc
masted <masted311@gmail.com>
2015-08-03 23:41:37 +0300

    Auto-commit on 04.08.2015 00:41:37

--- four/lib/CtrlThemeFourDd.class.php
+++ four/lib/CtrlThemeFourDd.class.php
@@ -26,8 +26,8 @@ abstract class CtrlThemeFourDd extends CtrlThemeFour {
     $this->d['blocksTpl'] = 'empty';
     $this->d['tpl'] = 'bookmarkContent';
     $this->d['contentTpl'] = 'dd/item';
-    $this->setPageTitle(' ');
-    $item = Misc::checkEmpty($this->items()->getItem($this->req->param(1)));
+    $this->d['item'] = $item = Misc::checkEmpty($this->items()->getItem($this->req->param(1)));
+    $this->setPageTitle($item['title'] ?: ' ');
     if ($item['title']) $this->setPageTitle($item['title']);
     $ddo = new Ddo($this->getStrName(), 'siteItem');
     $this->d['html'] = $ddo->setItem($item)->els();
--- four/tpl/dd/item.php
+++ four/tpl/dd/item.php
@@ -3,13 +3,15 @@
 </div>
 
 <script>
-  new Ngn.DdoItemEdit(
-    <?= $d['item']['id'] ?>,
-    new Element('div', {'class': 'itemPageEdit'}).inject(document.getElement('.col2 .bColBody'), 'top'),
-    {
-      onEditComplete: function(id) {
-        window.location.reload();
+  if (Ngn.authorized) {
+    new Ngn.DdoItemEdit(
+      <?= $d['item']['id'] ?>,
+      new Element('div', {'class': 'itemPageEdit'}).inject(document.getElement('.col2 .bColBody'), 'top'),
+      {
+        onEditComplete: function(id) {
+          window.location.reload();
+        }
       }
-    }
-  );
+    );
+  }
 </script>
